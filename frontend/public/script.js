9db69d7cb5c21a68ad57acfec89efa0c54b101a4
tieming <tiemings@usc.edu>
2020-04-15 15:20:17 -0700

    update frontend

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,9 +1,11 @@
 /**
- * Set the initial values of min_value and max_value
+ * Set the initial values of linear programming functions
  */
 function initialize() {
-  document.getElementById("min_value").value = "0";
-  document.getElementById("max_value").value = "100";
+  document.getElementById("objective_func").value = "3x + y";
+  document.getElementById("subject_to_1").value = "2x - y <= 4";
+  document.getElementById("subject_to_2").value = "2x + 3y <= 12";
+  document.getElementById("subject_to_3").value = "y <= 3";
 
   let loader = document.getElementById("loader");
   loader.style.display = "none";
@@ -29,9 +31,9 @@ async function submit() {
   document.body.style.cursor = "wait";
 
   // Accessing the div that has random value 
-  let random_value_element = document.getElementById("random-value");
+  let solution_lp = document.getElementById("result_by_lp_solver");
 
-  random_value_element.innerHTML = "Please wait...";
+  solution_lp.innerHTML = "Please wait...";
   
   // Show the loader element (spinning wheels)
   let loader = document.getElementById("loader");
@@ -39,14 +41,24 @@ async function submit() {
 
   try {
     // Get the min/max values from the user 
-    let min_value = document.getElementById("min_value").value;
-    let max_value = document.getElementById("max_value").value;
+    //let min_value = document.getElementById("min_value").value;
+    //let max_value = document.getElementById("max_value").value;
+    
+    let objective = document.getElementById("objective_func").value;
+    let subject1 = document.getElementById("subject_to_1").value;
+    let subject2 = document.getElementById("subject_to_2").value;
+    let subject3 = document.getElementById("subject_to_3").value;
+    let max_or_min = document.getElementById("optimize").value;
+    let integer_solution = document.getElementById("integer_solution").value;
+
+    //处理字符串 -> object
 
 
-    let request = `http://127.0.0.1:5000/?min_value=${min_value}&max_value=${max_value}`;
+    //let request = `http://127.0.0.1:5000/?min_value=${min_value}&max_value=${max_value}`;
+    let request = `http://127.0.0.1:5000/upload`
     console.log("request: ", request);
-
     // Send an HTTP GET request to the backend
+  
     const data = await axios.get(request);
 
     console.log("data.data: ", JSON.stringify(data.data, null, 2));
