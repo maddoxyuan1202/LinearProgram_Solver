53f69232c5ead5136c02fd4eaec6cf77d65963c0
tieming <tiemings@usc.edu>
2020-04-23 19:32:13 -0700

    gl-solver update

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -29,11 +29,17 @@ function openLP(evt, choose_solver) {
  * Math Prog for GLPK is multi-lined
  */
 function initialize() {
-  document.getElementById("objective_func").value = "3x + y";
-  document.getElementById("subject_to_1").value = "2x - y <= 4";
-  document.getElementById("subject_to_2").value = "2x + 3y <= 12";
-  document.getElementById("subject_to_3").value = "y <= 3";
-  document.getElementById("varibales").value = "x y";
+  document.getElementById("obj_1").value = 3;
+  document.getElementById("obj_2").value = 1;
+  document.getElementById("sub_1_1").value = 2;
+  document.getElementById("sub_1_2").value = -1;
+  document.getElementById("sub_1_3").value = 4;
+  document.getElementById("sub_2_1").value = 2;
+  document.getElementById("sub_2_2").value = 3;
+  document.getElementById("sub_2_3").value = 12;
+  document.getElementById("sub_3_1").value = 0;
+  document.getElementById("sub_3_2").value = 1;
+  document.getElementById("sub_3_3").value = 3;
   document.getElementById("input_glpk").value = 
   "var x1;\nvar x2;\nmaximize obj: 0.6 * x1 + 0.5 * x2;\ns.t. c1: x1 + 2 * x2 <= 1;\ns.t. c2: 3 * x1 + x2 <= 2;\nsolve;\ndisplay x1, x2;\nend;";
 
@@ -43,20 +49,6 @@ function initialize() {
 }
 
 initialize();
-
-/**
- * 通过正则表达式
- * 检查输入格式，返回JSON或者false，用在LP-solver中
- * This Function is used in submit_lp
- * Check whether the input is legal or not
- * if all inputs are legal, translate parameters into a JSON,
- * or return false
- */
-function input_to_json(variables, obj, sub1, sub2, sub3, sub4, sub5, sub6, max_or_min, integer){
-
-  return input_json;
-}
-
 
 /**
  * Handle the click event on Submit (Generate) button
@@ -91,43 +83,72 @@ async function submit_lpsolver() {
   loader.style.display = "inline-block";
 
   try {
-    let variables = document.getElementById("varibales").value;
-    let obj = document.getElementById("objective_func").value;
-    let sub1 = document.getElementById("subject_to_1").value;
-    let sub2 = document.getElementById("subject_to_2").value;
-    let sub3 = document.getElementById("subject_to_3").value;
-    let sub4 = document.getElementById("subject_to_4").value;
-    let sub5 = document.getElementById("subject_to_5").value;
-    let sub6 = document.getElementById("subject_to_6").value;
-    let max_or_min = document.getElementById("optimize").value;
-    let integer = document.getElementById("integer_solution").value;
+  let obj_1 = document.getElementById("obj_1").value;
+  let obj_2 = document.getElementById("obj_2").value;
+  
+  let sub_1_1 = document.getElementById("sub_1_1").value;
+  let sub_1_2 = document.getElementById("sub_1_2").value;
+  let sub_1_3 = document.getElementById("sub_1_3").value;
+  let sub_1_op = document.getElementById("sub_1_op").value;
+  let sub_2_1 = document.getElementById("sub_2_1").value;
+  let sub_2_2 = document.getElementById("sub_2_2").value;
+  let sub_2_3 = document.getElementById("sub_2_3").value;
+  let sub_2_op = document.getElementById("sub_2_op").value;
+  let sub_3_1 = document.getElementById("sub_3_1").value;
+  let sub_3_2 = document.getElementById("sub_3_2").value;
+  let sub_3_3 = document.getElementById("sub_3_3").value;
+  let sub_3_op = document.getElementById("sub_3_op").value;
+  let sub_4_1 = document.getElementById("sub_4_1").value;
+  let sub_4_2 = document.getElementById("sub_4_2").value;
+  let sub_4_3 = document.getElementById("sub_4_3").value;
+  let sub_4_op = document.getElementById("sub_4_op").value;
+  let sub_5_1 = document.getElementById("sub_5_1").value;
+  let sub_5_2 = document.getElementById("sub_5_2").value;
+  let sub_5_3 = document.getElementById("sub_5_3").value;
+  let sub_5_op = document.getElementById("sub_5_op").value;
+
+  let max_or_min = document.getElementById("optimize").value;
+  let integer = document.getElementById("integer_solution").value;
+  
+  let input_json = {};
+  input_json.opType = max_or_min;
+  input_json.optimize = 'z';
+  input_json.constraints = {'c1': {[sub_1_op]: sub_1_3}, 'c2': {[sub_2_op]: sub_2_3}, 'c3': {[sub_3_op]: sub_3_3}, 'c4': {[sub_4_op]: sub_4_3}, 'c5': {[sub_5_op]: sub_5_3}};
+  input_json.variables={
+    'x': {'z': obj_1, 'c1': sub_1_1, 'c2': sub_2_1, 'c3': sub_3_1, 'c4': sub_4_1, 'c5':sub_5_1},
+    'y': {'z': obj_2, 'c1': sub_1_2, 'c2': sub_2_2, 'c3': sub_3_2, 'c4': sub_4_2, 'c5':sub_5_2}};
+  if(integer === "integer_solution"){
+    input_json.ints = {'x': 1, 'y': 1};
+  }
+  input_json = JSON.stringify(input_json);
+
+  // // an example of input_json
+    // let input_json = {
+    //   optimize: 'maximize',
+    //   opType: 'max',
+    //   constraints: { c1: { max: 4 }, c2: { max: 12 }, c3: { max: 3 } },
+    //   variables: {
+    //     x: { maximize: 3, c1: 2, c2: 2, c3: 0 },
+    //     y: { maximize: 1, c1: -1, c2: 3, c3: 1 }
+    //   }
+    // };
+    // input_json = JSON.stringify(input_json);
 
 
-    // an example of input_json
-    let input_json = {
-      optimize: 'maximize',
-      opType: 'max',
-      constraints: { c1: { max: 4 }, c2: { max: 12 }, c3: { max: 3 } },
-      variables: {
-        x: { maximize: 3, c1: 2, c2: 2, c3: 0 },
-        y: { maximize: 1, c1: -1, c2: 3, c3: 1 }
-      }
-    };
-    input_json = JSON.stringify(input_json);
-    //let request_get = `http://127.0.0.1:5000/?model=${input_json}`;
-    //console.log("request via HTTP GET method: ", request_get);
-    let request_post = `http://127.0.0.1:5000/upload`;
-    console.log("request via HTTP POST method: ", request_post);
+    let request_get = `http://127.0.0.1:5000/?model=${input_json}`;
+    console.log("request via HTTP GET method: ", request_get);
+    //let request_post = `http://127.0.0.1:5000/upload`;
+    //console.log("request via HTTP POST method: ", request_post);
 
 
     // Send an HTTP GET request to the backend
-    //const data = await axios.get(request_get);
-    //console.log(data);
+    const data = await axios.get(request_get);
+    console.log(data);
 
     // Send an HTTP Post request to the backend
-    const data1 = await axios.post(request_post, input_json);
-    console.log(data1);
-    solution_lp.innerHTML = "Here is your solution: " + JSON.stringify(data1.data.output_lp);
+    //const data1 = await axios.post(request_post, input_json);
+    //console.log(data1);
+    solution_lp.innerHTML = "Here is your solution: " + JSON.stringify(data.data.output_lp);
     
   } catch (error) {
     console.log("error: ", error);
